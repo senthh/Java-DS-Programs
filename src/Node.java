782e9352e743b9b7e1fe07e5c5c9c826cf49f49a
senthilkumarb <senthilkumarb@cloudera.com>
2021-08-03 15:20:24 +0530

    Java DS Programs

---
+++ src/Node.java
@@ -0,0 +1,10 @@
+public class Node {
+    int data;
+    Node left;
+    Node right;
+    public Node(int value) {
+        this.data = value;
+        this.right = null;
+        this.right = null;
+    }
+}
